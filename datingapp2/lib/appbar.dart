3dda4d5d45744fa8b8ca76a2da44be118944cd08
0ujjwalmishra0 <ujjwalcoolguy05@gmail.com>
2021-04-05 16:39:31 +0530

    appbar

--- datingapp2/lib/appbar.dart
+++ datingapp2/lib/appbar.dart
@@ -1,10 +1,19 @@
-// import 'package:flutter/material.dart';
-// class MyAppBar extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return AppBar(
-//       leading: CircleAvatar(backgroundImage: ,),
+import 'package:flutter/material.dart';
+class MyAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      // leading: CircleAvatar(backgroundImage: ,),
+      actions: [
+        Image.asset('assets/1 (102).png'),
+        
+        Image.asset('assets/1 (102).png'),
+        
+        Image.asset('assets/1 (102).png'),
+        
+        Image.asset('assets/1 (102).png'),
+      ],
       
-//     );
-//   }
-// }
+    );
+  }
+}
